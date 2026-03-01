64c126349e45f71dd670068da6bb66ac4997837b
Pedro Mendes <pedrohmendesds@gmail.com>
2026-03-01 01:35:08 -0300

    WzLogo: prefer PNG (logo-wayzen.png) with SVG fallback

--- js/components/WzLogo.jsx
+++ js/components/WzLogo.jsx
@@ -5,7 +5,10 @@
 export default function WzLogo({ size = 40 }) {
   return (
     <div className="wz-logo" style={{ display: 'flex', alignItems: 'center', gap: 12 }}>
-      <img src="/assets/logo-wayzen.svg" alt="Logo Wayzen" style={{ height: size, width: 'auto', display: 'block' }} />
+      <picture>
+        <source srcSet="/assets/logo-wayzen.png" type="image/png" />
+        <img src="/assets/logo-wayzen.svg" alt="Logo Wayzen" style={{ height: size, width: 'auto', display: 'block' }} />
+      </picture>
     </div>
   );
 }
