e66d08b2f40a6d394ced9ac5f7f6816e7c466941
Pedro Mendes <pedrohmendesds@gmail.com>
2026-03-01 01:32:41 -0300

    Padronizar nome do logo para logo-wayzen.svg e atualizar WzLogo alt

--- js/components/WzLogo.jsx
+++ js/components/WzLogo.jsx
@@ -5,7 +5,7 @@
 export default function WzLogo({ size = 40 }) {
   return (
     <div className="wz-logo" style={{ display: 'flex', alignItems: 'center', gap: 12 }}>
-      <img src="/assets/WAYZEN LOGO DEFINITIVA.svg" alt="Wayzen" style={{ height: size, width: 'auto', display: 'block' }} />
+      <img src="/assets/logo-wayzen.svg" alt="Logo Wayzen" style={{ height: size, width: 'auto', display: 'block' }} />
     </div>
   );
 }
